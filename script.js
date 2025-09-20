25d3042ec7d9901370d933c9090deb471521ee2d
chouhanvikash <96488608+chouhanvikash@users.noreply.github.com>
2025-09-20 10:13:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+let btnSubmit=document.getElementById("submit");
+btnSubmit.addEventListener("click",(e)=>{
+	e.preventDefault();
+	let username=document.getElementById("username").value;
+	let password=document.getElementById("password").value;
+	alert(`Logged in as ${username}`);
+	
+	let checkbox=document.getElementById("checkbox");
+if(checkbox.checked){
+	
+	localStorage.setItem("username","password");
+	
+}
+	else{
+		localStorage.clear();
+		
+	}
+	if(localStorage.getItem("username"))
+	{
+		
+		let existing=document.getElementById("existing");
+		existing.textContent="Login as existing user";
+		existing.addEventListener("click",()=>{
+			alert(`Logged in as ${username}`);
+		});
+	}
+});
